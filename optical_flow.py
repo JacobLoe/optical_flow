4cc1174ee48af22650136e2361d0ceb88efd7caf
Christian Hentschel <chrstn.hntschl@gmail.com>
2020-08-05 14:30:08 +0200

    added IdMapper to fetch video filenames by sha256 ids - needs /root/file_mappings.tsv refactoring

--- optical_flow.py
+++ optical_flow.py
@@ -7,12 +7,16 @@ from tqdm import tqdm
 import shutil
 from scipy.spatial.distance import euclidean
 
+from idmapper import  TSVIdMapper
 
 BINS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]     #
 ANGLE_BINS = [0, 45, 90, 135, 180, 225, 270, 315, 360]
-VERSION = '20200715'      # the version of the script
+VERSION = '20200804'      # the version of the script
 aggregate = np.mean
 
+
+# FIXME: class version for extractors
+# FIXME: replace prints by logger
 
 def bin_values(value, bins):
 
@@ -35,11 +39,12 @@ def resize_frame(frame, frame_width):
 
 
 def read_frame(vid, timestamp, frame_width):
+    # hint: use nframes = vid.get(cv2.CAP_PROP_FRAME_COUNT)
     vid.set(cv2.CAP_PROP_POS_FRAMES, timestamp)
     ret, frame = vid.read()  # if ret is false, frame has no content
 
     if not ret:
-        return ret, 0
+        return ret, None 
 
     if frame_width:  # reshape the frame according to the given frame width and the aspect ratio od the movie
         frame = resize_frame(frame, frame_width)
@@ -71,40 +76,75 @@ def calculate_optical_flow(frame1, frame2):
 def get_optical_flow(v_path, frame_width):
 
     vid = cv2.VideoCapture(v_path)
-    summed_mags = []    # list of summed up magnitudes
-    timestamps = []         # list of timestamps, corresponding to summed_mags
-    angles_histogram_list = []
-    step_size_in_frames = int(vid.get(cv2.CAP_PROP_FPS)*step_size/1000)  # convert the step_size and window_size from ms to frames, dependent on the fps of the movie
-    window_size_in_frames = int(vid.get(cv2.CAP_PROP_FPS)*window_size/1000)
-    timestamp_frames = max(0, 0-int(window_size_in_frames/2))    # timestamp iterator, counted in frames
+    if not vid.isOpened():
+        raise IOError("Unable to read from video: '{v_path}'".format(v_path=v_path))
 
-    timestamps.append((int(timestamp_frames/vid.get(cv2.CAP_PROP_FPS)*1000)))
+    tot_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = vid.get(cv2.CAP_PROP_FPS) 
+    step_size_in_frames = int(fps*step_size/1000)  # convert the step_size and window_size from ms to frames, dependent on the fps of the movie
+    window_size_in_frames = int(fps*window_size/1000)
+ 
+    windows = [( max(0,c-int(window_size_in_frames/2.)), min(tot_frames-1,c+int(window_size_in_frames/2.)) ) for c in range(0,tot_frames,step_size_in_frames)]
 
-    # go through the video and extract two frames, one at the current timestamp, the second at timestamp+window_size
-    # save the optical flow at each timestamp
-    while vid.isOpened():
-        # read the frame at the current timestamp, stop the reading if the video is finished
-        ret, curr_frame = read_frame(vid, timestamp_frames - int(window_size_in_frames/2), frame_width)
-        if not ret:
-            break
-        # read the "future" frame, at the end of the window, stop the reading if the video is finished
-        ret, future_frame = read_frame(vid, timestamp_frames + int(window_size_in_frames/2), frame_width)
-        if not ret:
-            break
-        # calculate the optical flow for the current and the future frame, return the summed up magnitudes and a histogram for the angles found between the frames
-        mag = calculate_optical_flow(curr_frame, future_frame)
+    mags = list()
+    for start, end in windows:
+        if not vid.isOpened():
+            raise IOError("Unable to read from video: '{v_path}'".format(v_path=v_path))
+        
+        ret, start_frame = read_frame(vid, start, frame_width)
+        if not ret or start_frame is None:
+            raise IOError("Unable to read start frame {f} from video: '{v_path}'".format(f=start, v_path=v_path))
 
-        # save the two optical flow components
-        summed_mags.append(mag)
-        # move along the video according to the step_size
-        timestamp_frames += step_size_in_frames
+        ret, end_frame = read_frame(vid, end, frame_width)
+        
+        if not ret or end_frame is None:
+            raise IOError("Unable to read end frame {f} from video: '{v_path}'".format(f=end, v_path=v_path))
 
-    timestamps.append((int(timestamp_frames/vid.get(cv2.CAP_PROP_FPS)*1000)))
+        mag = calculate_optical_flow(start_frame, end_frame)
+        mags.append((start,end,mag))
 
     vid.release()
     cv2.destroyAllWindows()
 
-    return summed_mags, timestamps
+    agg_mags = list()
+    for pos in range(0,tot_frames,step_size_in_frames):
+        agg_mag = [mag[2] for mag in mags if pos>=mag[0] and pos<mag[1]]
+        if len(agg_mag)>0:
+            agg_mags.append((pos, np.mean(agg_mag)))
+        else:
+            print("WARN: no entry for pos={pos}".format(pos=pos))
+
+    start_ms = int(agg_mags[0][0]/fps*1000)
+    end_ms = int(agg_mags[-1][0]/fps*1000)
+
+    return [mag[1] for mag in agg_mags], [start_ms, end_ms]
+
+	
+#    # go through the video and extract two frames, one at the current timestamp, the second at timestamp+window_size
+#    # save the optical flow at each timestamp
+#    while vid.isOpened():
+#        # read the frame at the current timestamp, stop the reading if the video is finished
+#        ret, curr_frame = read_frame(vid, timestamp_frames - int(window_size_in_frames/2), frame_width)
+#        if not ret:
+#            break
+#        # read the "future" frame, at the end of the window, stop the reading if the video is finished
+#        ret, future_frame = read_frame(vid, timestamp_frames + int(window_size_in_frames/2), frame_width)
+#        if not ret:
+#            break
+#        # calculate the optical flow for the current and the future frame, return the summed up magnitudes and a histogram for the angles found between the frames
+#        mag = calculate_optical_flow(curr_frame, future_frame)
+#
+#        # save the two optical flow components
+#        summed_mags.append(mag)
+#        # move along the video according to the step_size
+#        timestamp_frames += step_size_in_frames
+#
+#    timestamps.append((int(timestamp_frames/vid.get(cv2.CAP_PROP_FPS)*1000)))
+#
+#    vid.release()
+#    cv2.destroyAllWindows()
+#
+#    return summed_mags, timestamps
 
 
 def aggregate_segments(summed_mags):
@@ -144,64 +184,69 @@ def scale_magnitudes(mag, top_percentile):
 
 
 def write_mag_to_csv(f_path, mag, segment_timestamps):
-    if not os.path.isdir(os.path.join(f_path, 'optical_flow')):
-        os.makedirs(os.path.join(f_path, 'optical_flow'))
+    #if not os.path.isdir(os.path.join(f_path, 'optical_flow')):
+    #    os.makedirs(os.path.join(f_path, 'optical_flow'))
 
-    mag_csv_path = os.path.join(f_path, 'optical_flow/mag_optical_flow_{}.csv'.format(os.path.split(f_path)[1]))
+    #mag_csv_path = os.path.join(f_path, 'optical_flow/mag_optical_flow_{}.csv'.format(os.path.split(f_path)[1]))
 
-    with open(mag_csv_path, 'w', newline='') as f:
+    with open(f_path, 'w', newline='') as f:
         mag = str(mag).strip('[').strip(']').replace(',', ' ')
         line = str(segment_timestamps[0]) + '\t' + str(segment_timestamps[1]) + '\t' + mag
         f.write(line)
 
 
-def main(videos_path, features_path, frame_width):
-    list_videos_path = glob.glob(os.path.join(videos_path, '**/*.mp4'), recursive=True)  # get the list of videos in videos_dir
-    cp = os.path.commonprefix(list_videos_path)  # get the common dir between paths found with glob
+def main(videos_path, features_path, videoids, idmapper, frame_width=129, step_size=300, window_size=300):
+    #list_videos_path = glob.glob(os.path.join(videos_path, '**/*.mp4'), recursive=True)  # get the list of videos in videos_dir
+    #cp = os.path.commonprefix(list_videos_path)  # get the common dir between paths found with glob
 
-    list_features_path = [os.path.join(
-                         os.path.join(features_path,
-                         os.path.relpath(p, cp))[:-4])
-                         for p in list_videos_path]  # create a list of paths where all the data (segment-detection,frames,features) are saved to
+    #list_features_path = [os.path.join(
+    #                     os.path.join(features_path,
+    #                     os.path.relpath(p, cp))[:-4])
+    #                     for p in list_videos_path]  # create a list of paths where all the data (segment-detection,frames,features) are saved to
 
     done = 0
-    while done < len(list_videos_path):
-        for v_path, f_path in tqdm(zip(list_videos_path, list_features_path), total=len(list_videos_path)):
+    while done < len(videoids):
+        for videoid in tqdm(videoids):
+            try:
+                video_rel_path = idmapper.get_filename(videoid)
+            except KeyError as err:
+                print("No such videoid: '{videoid}'".format(videoid=videoid))
+                done += 1
 
-            of_path = os.path.join(f_path, 'optical_flow')
-            done_file_path = os.path.join(of_path, '.done')
-            video_name = os.path.split(v_path)[1]
+            print(videoid, os.path.basename(video_rel_path))
 
-            if not os.path.isdir(of_path):
-                os.makedirs(of_path)
+            #FIXME: extractor as class, "opticalflow" as property, version as property
+            features_fname = "{videoid}.opticalflow.csv".format(videoid=videoid)
+            features_fname = "{video_fname}.opticalflow.csv".format(video_fname=os.path.basename(video_rel_path))
+            f_path = os.path.join(features_path, features_fname)
+            done_file_path = os.path.join(features_path, '.'+features_fname)
 
-                print('get angles and magnitudes')
+            v_path = os.path.join(videos_path, video_rel_path)
+
+            if not os.path.isfile(done_file_path) or not open(done_file_path, 'r').read() == VERSION:
+                print("Optical flow results missing or version did not match")
+
+                print('Compute angles and magnitudes')
                 summed_mags, timestamps = get_optical_flow(v_path, frame_width)
 
-                print('aggregate segments')
-                aggregated_segments = aggregate_segments(summed_mags)
+                #print('aggregate segments')
+                #aggregated_segments = aggregate_segments(summed_mags)
+                aggregated_segments = summed_mags
 
                 scaled_segments = scale_magnitudes(aggregated_segments, top_percentile)
 
-                print('write results to csv')
+                print('Write results to csv')
                 write_mag_to_csv(f_path, scaled_segments, timestamps)
 
                 # create a hidden file to signal that the optical flow for a movie is done
                 # write the current version of the script in the file
-                with open(done_file_path, 'a') as d:
+                with open(done_file_path, 'w') as d:
                     d.write(VERSION)
                 done += 1  # count the instances of the optical flow done correctly
-            # do nothing if a .done-file exists and the versions in the file and the script match
-            elif os.path.isfile(done_file_path) and open(done_file_path, 'r').read() == VERSION:
+            else:
+                # do nothing if a .done-file exists and the versions in the file and the script match
                 done += 1  # count the instances of the optical flow done correctly
-                print('optical flow was already done for {}'.format(video_name))
-            # if the folder already exists but the .done-file doesn't, delete the folder
-            elif os.path.isfile(done_file_path) and not open(done_file_path, 'r').read() == VERSION:
-                shutil.rmtree(of_path)
-                print('versions did not match for {}'.format(video_name))
-            elif not os.path.isfile(done_file_path):
-                shutil.rmtree(of_path)
-                print('optical flow was not done correctly for {}'.format(video_name))
+                print('optical flow was already done for {video}'.format(video=video_rel_path))
 
 
 if __name__ == "__main__":
@@ -209,6 +254,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("videos_dir", help="the directory where the video-files are stored")
     parser.add_argument("features_dir", help="the directory where the images are to be stored")
+    parser.add_argument("videoids", help="List of video ids", nargs='+')
     parser.add_argument("--frame_width", type=int, default=129, help="set the width at which to which the frames are rescaled, default is 129")
     parser.add_argument("--step_size", type=int, default=300, help="defines at which distances the optical flow is calculated, in milliseconds, default is 300")
     parser.add_argument("--window_size", type=int, default=300,
@@ -222,4 +268,6 @@ if __name__ == "__main__":
     window_size = args.window_size
     top_percentile = args.top_percentile
 
-    main(args.videos_dir, args.features_dir, args.frame_width)
+    # FIXME: make more generic once workflow is setup
+    idmapper = TSVIdMapper('/root/file_mappings.tsv')
+    main(args.videos_dir, args.features_dir, args.videoids, idmapper, args.frame_width, args.step_size, args.window_size)
